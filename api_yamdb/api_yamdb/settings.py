9b7b9f43afeac9015bccd56c7380dcf6f4506f69
Олег Жугрин <o43zhu@gmail.com>
2023-04-26 22:12:37 +0300

    jwt

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -90,9 +90,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # REST
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ],
 }
 
 # Internationalization
