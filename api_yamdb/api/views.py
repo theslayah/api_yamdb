f96ccc5d9a958d7a32d97c35b54b2fb969a4e05b
Олег Жугрин <o43zhu@gmail.com>
2023-04-27 16:07:04 +0300

    AdminUser+SomeCorrtections

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
+from django.conf import settings
 from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework import filters, permissions, status, viewsets
+from rest_framework import filters, status, viewsets
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+class AdminUser(admin.ModelAdmin):
+    """Настройка админки для пользователей."""
+    list_display = ('username', 'email', 'first_name', 'last_name',
+                    'bio', 'role', 'confirmation_code')
+    search_fields = ('username',)
+    list_filter = ('role',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(User, AdminUser)
